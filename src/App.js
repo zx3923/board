e8ee727c29ccdd9ce07520826104a5b2d6ee8c5e
zx3923 <seop3923@gmail.com>
2022-08-31 18:35:30 +0900

    학원1차

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import BoardList from "./components/BoardList";
 import Write from "./components/Write";
+import axios, { Axios } from "axios";
 
 function App() {
+  const onInsert = async () => {
+    await axios({ url: "http://localhost:8000/list/", method: "get" }).then(
+      () => {
+        alert("확인");
+      }
+    );
+  };
   return (
     <>
       <BoardList></BoardList>
-      <Write></Write>
+      <Write onInsert={onInsert}></Write>
     </>
   );
 }
--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -1,48 +1,41 @@
-import React from 'react';
-import styels from './BoardList.module.css';
-import { Axios } from 'axios';
+import React, { Component } from "react";
+import styels from "./BoardList.module.css";
 
 const BoardList = () => {
-    const submitTest = () => {
-        Axios.get("http://localhost:8000/", {}).then(() => {
-            alert("등록 완료!");
-        });
-    };
-
-    return (
-        <>
-            <div className={styels.table}>
-                <div className={styels.t_head}>
-                    <div>선택</div>
-                    <div>번호</div>
-                    <div>제목</div>
-                    <div>작성자</div>
-                    <div>작성일</div>
-                </div>
-                <div className={styels.t_body}>
-                    <div className={styels.body_list}>
-                        <input type="checkbox" />
-                        <div>1</div>
-                        <div>게시글1</div>
-                        <div>홍길동</div>
-                        <div>2022-08-30</div>
-                    </div>
-                    <div className={styels.body_list}>
-                        <input type="checkbox" />
-                        <div>2</div>
-                        <div>게시글2</div>
-                        <div>홍길순</div>
-                        <div>2022-08-30</div>
-                    </div>
-                </div>
-            </div>
-            <div className={styels.btn_box}>
-                <button onClick={submitTest}>글 쓰기</button>
-                <button>수정하기</button>
-                <button>삭제하기</button>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={styels.table}>
+        <div className={styels.t_head}>
+          <div>선택</div>
+          <div>번호</div>
+          <div>제목</div>
+          <div>작성자</div>
+          <div>작성일</div>
+        </div>
+        <div className={styels.t_body}>
+          <div className={styels.body_list}>
+            <input type="checkbox" />
+            <div>1</div>
+            <div>게시글1</div>
+            <div>홍길동</div>
+            <div>2022-08-30</div>
+          </div>
+          <div className={styels.body_list}>
+            <input type="checkbox" />
+            <div>2</div>
+            <div>게시글2</div>
+            <div>홍길순</div>
+            <div>2022-08-30</div>
+          </div>
+        </div>
+      </div>
+      <div className={styels.btn_box}>
+        <button>글 쓰기</button>
+        <button>수정하기</button>
+        <button>삭제하기</button>
+      </div>
+    </>
+  );
 };
 
 export default BoardList;
